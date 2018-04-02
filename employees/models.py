e290c478ce96f36f279dae7239122c891f714042
sheilambadi <mbadiatieno@gmail.com>
2018-04-02 18:22:53 +0300

    add employees model

--- employees/models.py
+++ employees/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class employees(models.Model):
+    firstname = models.CharField(max_length=15)
+    lastname = models.CharField(max_length=15)
+    emp_id = models.IntegerField()
+
+    def __str__(self):
+        return self.firstname
+
+     #remove additional s in admin panel
+    class Meta:
+        verbose_name_plural = 'Employees'
